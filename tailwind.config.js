f6d47bf3a87185dca1eac8e1720653624ed24a9c
Tanmay <tanmaychandgude.007@gmail.com>
2025-04-23 19:34:52 +0530

    Initial commit of existing code

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+export default {
+  content: [
+    "./index.html",
+    "./**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#FFD700', // Yellow from logo
+        secondary: '#000000', // Black from logo
+      },
+    },
+  },
+  plugins: [],
+}
